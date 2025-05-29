c511a9b7b189e63d6f0c1e6e0c0f9f57ae080db1
Youssuf Shakweh <yousef.shakwa@gmail.com>
2025-05-30 00:23:20 +0300

    Implement exists function to check user existance

--- auth.py
+++ auth.py
@@ -1,4 +1,16 @@
 class User:
-    def __init__(self, username, password):
+    users = [
+        {"username": "youssuf", "password": "123qwe12"},
+        {"username": "hussain", "password": "123qwe12"},
+    ]
+
+    def __init__(self, username: str, password: str):
         self.username = username
         self.password = password
+
+    def exists(self, username: str) -> bool:
+        for user in self.users:
+            if username == user["username"]:
+                return True
+
+        return False
